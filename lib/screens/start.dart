552928dfc9e7200fccc87f81ea04d97fa24eb174
comerc <andrew.kachanov@gmail.com>
2020-11-21 15:11:35 +0300

    Improve StartScreen

--- lib/screens/start.dart
+++ lib/screens/start.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pet_finder/import.dart';
 
 class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    Widget result = Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        leading: Container(),
         brightness: Brightness.light,
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
       body: StartBody(),
     );
+    result = WillPopScope(
+      onWillPop: _onWillPop,
+      child: result,
+    );
+    return result;
+  }
+
+  Future<bool> _onWillPop() async {
+    // ignore: unawaited_futures
+    SystemNavigator.pop();
+    return false;
   }
 }
 
@@ -27,40 +40,10 @@ class _StartBodyState extends State<StartBody> {
   @override
   void initState() {
     super.initState();
-    load(() => getBloc<ProfileCubit>(context).load());
   }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<ProfileCubit, ProfileState>(
-      listenWhen: (ProfileState previous, ProfileState current) {
-        return current.status == ProfileStatus.ready;
-      },
-      listener: (BuildContext context, ProfileState state) {
-        Future.delayed(Duration(milliseconds: 300)).then((_) {
-          navigator.pop();
-        });
-      },
-      builder: (BuildContext context, ProfileState state) {
-        final cases = {
-          ProfileStatus.initial: () => Container(),
-          ProfileStatus.loading: () =>
-              Center(child: CircularProgressIndicator()),
-          ProfileStatus.error: () {
-            return Center(
-                child: FloatingActionButton(
-              onPressed: () {
-                BotToast.cleanAll();
-                load(() => getBloc<ProfileCubit>(context).load());
-              },
-              child: Icon(Icons.replay),
-            ));
-          },
-          ProfileStatus.ready: () => Center(child: Text('Profile loaded...')),
-        };
-        assert(cases.length == ProfileStatus.values.length);
-        return cases[state.status]();
-      },
-    );
+    return Center(child: Text('Start'));
   }
 }
