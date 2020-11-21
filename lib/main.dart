04c223272f5f32e5e30c8bd304c2926accad7397
comerc <andrew.kachanov@gmail.com>
2020-11-21 23:10:16 +0300

    Prepare for auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,45 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:equatable/equatable.dart';
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:bot_toast/bot_toast.dart';
+// import 'package:flutter/scheduler.dart' show timeDilation;
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_finder/import.dart';
 
 void main() {
-  runApp(App(
-    databaseRepository: DatabaseRepository(),
-  ));
+  // timeDilation = 10.0; // Will slow down animations by a factor of two
+  // debugPaintSizeEnabled = true;
+  // FlutterError.onError = (FlutterErrorDetails details) {
+  //   if (kDebugMode) {
+  //     // In development mode, simply print to console.
+  //     FlutterError.dumpErrorToConsole(details);
+  //   } else {
+  //     // In production mode, report to the application zone to report to
+  //     // Sentry.
+  //     Zone.current.handleUncaughtError(details.exception, details.stack);
+  //   }
+  // };
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // await Firebase.initializeApp();
+    EquatableConfig.stringify = kDebugMode;
+    // Bloc.observer = SimpleBlocObserver();
+    HydratedBloc.storage = await HydratedStorage.build();
+    runApp(
+      App(
+        // authenticationRepository: AuthenticationRepository(),
+        databaseRepository: DatabaseRepository(),
+      ),
+    );
+  }, (error, stackTrace) {
+    // Whenever an error occurs, call the `_reportError` function. This sends
+    // Dart errors to the dev console or Sentry depending on the environment.
+    // _reportError(error, stackTrace);
+  });
 }
 
 final navigatorKey = GlobalKey<NavigatorState>();
@@ -18,26 +49,49 @@ NavigatorState get navigator => navigatorKey.currentState;
 class App extends StatelessWidget {
   App({
     Key key,
+    // @required this.authenticationRepository,
     @required this.databaseRepository,
-  })  : assert(databaseRepository != null),
+  })  :
+        // assert(authenticationRepository != null),
+        assert(databaseRepository != null),
         super(key: key);
 
+  // final AuthenticationRepository authenticationRepository;
   final DatabaseRepository databaseRepository;
 
   @override
   Widget build(BuildContext context) {
-    // databaseRepository
-    //     .readUnits(categoryId: 'cat', limit: 4)
-    //     .then((List<UnitModel> value) => out(value));
-    // databaseRepository
-    //     .readNewestUnits(limit: 4)
-    //     .then((List<UnitModel> value) => out(value));
-    // databaseRepository
-    //     .readCategories()
-    //     .then((List<CategoryModel> value) => out(value[0]));
-    // databaseRepository
-    //     .readProfile()
-    //     .then((ProfileModel value) => out(value.wishes[1].unitId));
+    Widget result = AppView();
+    result = MultiBlocProvider(
+      providers: [
+        // BlocProvider(
+        //   create: (BuildContext context) =>
+        //       AuthenticationCubit(authenticationRepository),
+        // ),
+        BlocProvider(
+          create: (BuildContext context) => AppCubit(databaseRepository),
+        ),
+      ],
+      child: result,
+    );
+    result = MultiRepositoryProvider(
+      providers: [
+        // RepositoryProvider.value(
+        //   value: authenticationRepository,
+        // ),
+        RepositoryProvider.value(
+          value: databaseRepository,
+        ),
+      ],
+      child: result,
+    );
+    return result;
+  }
+}
+
+class AppView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pet Finder',
       theme: ThemeData(
@@ -58,15 +112,28 @@ class App extends StatelessWidget {
       ],
       builder: (BuildContext context, Widget child) {
         Widget result = child;
-        result = BlocProvider(
-          create: (BuildContext context) =>
-              AppCubit(getRepository<DatabaseRepository>(context)),
-          child: result,
-        );
-        result = RepositoryProvider(
-          create: (BuildContext context) => databaseRepository,
-          child: result,
-        );
+        // result = BlocListener<AuthenticationCubit, AuthenticationState>(
+        //   listener: (BuildContext context, AuthenticationState state) {
+        //     final cases = {
+        //       AuthenticationStatus.authenticated: () {
+        //         navigator.pushAndRemoveUntil<void>(
+        //           HomeScreen().getRoute(),
+        //           (Route route) => false,
+        //         );
+        //       },
+        //       AuthenticationStatus.unauthenticated: () {
+        //         navigator.pushAndRemoveUntil<void>(
+        //           LoginScreen().getRoute(),
+        //           (Route route) => false,
+        //         );
+        //       },
+        //       AuthenticationStatus.unknown: () {},
+        //     };
+        //     assert(cases.length == AuthenticationStatus.values.length);
+        //     cases[state.status]();
+        //   },
+        //   child: result,
+        // );
         result = BotToastInit()(context, result);
         return result;
       },
@@ -81,8 +148,10 @@ class App extends StatelessWidget {
       //     name: 'Dogs',
       //     totalOf: 210,
       //     color: '#90caf9',
+      //     breeds: [],
       //   ),
       // ),
+      // onGenerateRoute: (_) => SplashScreen().getRoute(),
     );
   }
 }
