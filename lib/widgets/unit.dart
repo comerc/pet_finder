bd52bbd753528877e575284ec29c5bdf610920d7
comerc <andrew.kachanov@gmail.com>
2022-03-24 18:01:28 +0300

    Fix InkWell

--- lib/widgets/unit.dart
+++ lib/widgets/unit.dart
@@ -17,199 +17,60 @@ class Unit extends StatefulWidget {
 class _UnitState extends State<Unit> {
   @override
   Widget build(BuildContext context) {
-    final unit = widget.unit;
-    // TODO: InkWell
-    final width = MediaQuery.of(context).size.width;
-    return GestureDetector(
-      onLongPress: () {}, // чтобы сократить время для splashColor
-      onTap: () {
-        navigator.push(UnitScreen(unit: unit).getRoute());
-      },
-      child: Container(
-        padding: EdgeInsets.only(
-          top: 16,
-          // left: 16,
-          // right: 16,
+    return Column(
+      children: [
+        SizedBox(
+          height: 16,
         ),
-        color: Theme.of(context).scaffoldBackgroundColor,
-        // margin: EdgeInsets.only(
-        //   right: index == null ? 0 : 16,
-        //   left: index == 0 ? 16 : 0,
-        //   bottom: 16,
-        // ),
-        // width: 220,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              height: width,
-              child: Stack(
-                children: [
-                  _buildImage(),
-                  Positioned(
-                    top: 0,
-                    left: 0,
-                    child: _buildBadge(),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: _buildTitle(),
-                  ),
-                  // Align(
-                  //   alignment: Alignment.topRight,
-                  //   child: Padding(
-                  //     padding: EdgeInsets.all(12),
-                  //     child: BlocBuilder<ProfileCubit, ProfileState>(
-                  //       buildWhen: (
-                  //         ProfileState previous,
-                  //         ProfileState current,
-                  //       ) {
-                  //         return previous.wishes != current.wishes;
-                  //       },
-                  //       builder: (
-                  //         BuildContext context,
-                  //         ProfileState state,
-                  //       ) {
-                  //         final isWished =
-                  //             state.status == ProfileStatus.ready &&
-                  //                 state.wishes.indexWhere((WishModel wish) {
-                  //                       return wish.unit.id == unit.id;
-                  //                     }) >
-                  //                     -1;
-                  //         return GestureDetector(
-                  //           onLongPress:
-                  //               () {}, // чтобы сократить время для splashColor
-                  //           onTap: () {
-                  //             save(
-                  //               () => getBloc<ProfileCubit>(context).saveWish(
-                  //                 WishData(
-                  //                   unitId: unit.id,
-                  //                   value: !isWished,
-                  //                 ),
-                  //               ),
-                  //             );
-                  //           },
-                  //           child: Container(
-                  //             height: 30,
-                  //             width: 30,
-                  //             decoration: BoxDecoration(
-                  //               shape: BoxShape.circle,
-                  //               color: isWished
-                  //                   ? Colors.red.shade400
-                  //                   : Colors.white,
-                  //             ),
-                  //             child: Icon(
-                  //               Icons.favorite,
-                  //               size: 16,
-                  //               color: isWished
-                  //                   ? Colors.white
-                  //                   : Colors.grey.shade300,
-                  //             ),
-                  //           ),
-                  //         );
-                  //       },
-                  //     ),
-                  //   ),
-                  // ),
-                ],
+        _buildImage(
+          child: Stack(
+            children: [
+              Positioned(
+                top: 0,
+                left: 0,
+                child: _buildBadge(),
               ),
-            ),
-            _buildBottom(),
-            // Padding(
-            //   padding: EdgeInsets.all(16),
-            //   child: Column(
-            //     // crossAxisAlignment: CrossAxisAlignment.start,
-            //     children: [
-            //       // Container(
-            //       //   decoration: BoxDecoration(
-            //       //     color: getConditionBackgroundColor(unit.condition),
-            //       //     borderRadius: BorderRadius.all(
-            //       //       Radius.circular(10),
-            //       //     ),
-            //       //   ),
-            //       //   padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-            //       //   child: Text(
-            //       //     getConditionName(unit.condition),
-            //       //     style: TextStyle(
-            //       //       color: getConditionForegroundColor(unit.condition),
-            //       //       fontWeight: FontWeight.bold,
-            //       //       fontSize: 12,
-            //       //     ),
-            //       //   ),
-            //       // ),
-            //       // SizedBox(
-            //       //   height: 8,
-            //       // ),
-            //       // Text(
-            //       //   unit.breed.name,
-            //       //   style: TextStyle(
-            //       //     color: Colors.grey.shade800,
-            //       //     fontSize: 18,
-            //       //     fontWeight: FontWeight.bold,
-            //       //   ),
-            //       // ),
-            //       // SizedBox(
-            //       //   height: 8,
-            //       // ),
-            //       Row(
-            //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //         children: [
-            //           Icon(
-            //             Icons.location_on,
-            //             color: Colors.grey.shade600,
-            //             size: 18,
-            //           ),
-            //           SizedBox(
-            //             width: 4,
-            //           ),
-            //           Text(
-            //             unit.address,
-            //             style: TextStyle(
-            //               color: Colors.grey.shade600,
-            //               fontSize: 12,
-            //             ),
-            //           ),
-            //           //Expanded(child: child)
-            //           Text("1234"),
-            //           // SizedBox(
-            //           //   width: 4,
-            //           // ),
-            //           // Text(
-            //           //   "(" + unit.location + "km)",
-            //           //   style: TextStyle(
-            //           //     color: Colors.grey.shade600,
-            //           //     fontSize: 12,
-            //           //     fontWeight: FontWeight.bold,
-            //           //   ),
-            //           // ),
-            //         ],
-            //       ),
-            //     ],
-            //   ),
-            // ),
-          ],
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: _buildTitle(),
+              ),
+            ],
+          ),
         ),
-      ),
+        _buildBottom(),
+      ],
     );
   }
 
-  Widget _buildImage() {
+  Widget _buildImage({required Widget child}) {
     final unit = widget.unit;
+    final width = MediaQuery.of(context).size.width;
     return Hero(
       tag: unit.imageUrl,
       child: Container(
+        width: width,
+        height: width,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: getImageProvider(unit.imageUrl),
             // TODO: вертикальные фотки не обрезать, а добавлять поля
             fit: BoxFit.cover,
           ),
-          // borderRadius: BorderRadius.only(
-          //   topLeft: Radius.circular(19),
-          //   topRight: Radius.circular(19),
-          // ),
+        ),
+        child: Material(
+          type: MaterialType.transparency,
+          child: InkWell(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.white.withOpacity(0.24),
+            onLongPress: () {}, // чтобы сократить время для splashColor
+            onTap: () {
+              navigator.push(UnitScreen(unit: unit).getRoute());
+            },
+            child: child,
+          ),
+          color: Colors.transparent,
         ),
       ),
     );
