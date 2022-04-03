909a3e7b3ba4ad7c8616bfe28b9554171508ac9d
comerc <andrew.kachanov@gmail.com>
2022-04-03 20:22:42 +0300

    Add WishModel

---
+++ lib/models/wish.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:equatable/equatable.dart';
+import 'package:pet_finder/import.dart';
+
+part 'wish.g.dart';
+
+@JsonSerializable()
+class WishModel extends Equatable {
+  WishModel({
+    required this.unit,
+  });
+
+  final UnitModel unit;
+
+  @override
+  List<Object> get props => [
+        unit,
+      ];
+
+  static WishModel fromJson(Map<String, dynamic> json) =>
+      _$WishModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$WishModelToJson(this);
+}
